b8d63148970936b6ddaeddfcffddf85417fbd24a
Afa Madza <305@holbertonschool.com>
2018-06-10 22:19:24 +0000

    Added gitignore and user.py

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the class User, which inherits from
+BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User - class that inherits from BaseModel
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
