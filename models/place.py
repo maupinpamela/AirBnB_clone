dee427314a62e68777cea145d9e1bcbcb5e91a7c
maupinpamela <maupinpamela@gmail.com>
2018-06-12 22:45:47 +0000

    Added doc string info

--- models/place.py
+++ models/place.py
@@ -9,6 +9,18 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     """
     Place - class that inherits from BaseModel
+    Public Class Attributes:
+        city_id: string - empty string : it will be the City.id
+        user_id: string - empty string: it will be the User.id
+        name: string - empty string
+        description: string - empty string
+        number_rooms: integer - 0
+        number_bathrooms: integer - 0
+        max_guest: integer - 0
+        price_by_night: integer - 0
+        latitude: float - 0.0
+        longitude: float - 0.0
+        amenity_ids: list of string - empty list
     """
     city_id = ""
     user_id = ""
