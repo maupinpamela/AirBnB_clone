7b3cd73a877a6069ed4d924dd160bad211674697
maupinpamela <maupinpamela@gmail.com>
2018-06-12 22:45:58 +0000

    Added doc string info

--- models/review.py
+++ models/review.py
@@ -9,6 +9,10 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """
     Review - class that inherits from BaseModel
+    Public Class Attributes:
+        place_id: string - empty string: it will be the Place.id
+        user_id: string - empty string: it will be the User.id
+        text: string - empty string
     """
     place_id = ""
     user_id = ""
