156afe1c75e020033528170284eefae104ad9c7f
maupinpamela <maupinpamela@gmail.com>
2018-06-12 22:45:21 +0000

    Added doc string info

--- models/city.py
+++ models/city.py
@@ -9,6 +9,9 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """
     City - class that inherits from BaseModel
+    Public Class Attributes:
+        state_id: string - empty string: it will be the State.id
+        name: string - empty string
     """
     state_id = ""
     name = ""
