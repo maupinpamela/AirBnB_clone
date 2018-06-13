3453bdfe14063f981ce8fd06a531317658b13106
Afa Madza <305@holbertonschool.com>
2018-06-13 22:55:44 +0000

    Refactored all function

--- console.py
+++ console.py
@@ -36,6 +36,7 @@ class HBNBCommand(cmd.Cmd):
         args = shlex.split(args)
         if len(args) == 0:
             print("** class name missing **")
+            return
         else:
             check = 0
             for i in range(len(HBNBCommand.class_names)):
@@ -46,6 +47,7 @@ class HBNBCommand(cmd.Cmd):
                     check = 1
             if check == 0:
                 print("** class doesn't exist **")
+                return
 
     def do_show(self, args):
         """ Prints the string of an instance based on class name and id"""
@@ -53,14 +55,17 @@ class HBNBCommand(cmd.Cmd):
         args = shlex.split(args)
         if len(args) == 0:
             print("** class name missing **")
+            return
         check = 0
         for i in range(len(HBNBCommand.class_names)):
             if args[0] == HBNBCommand.class_names[i]:
                 check = 1
         if check == 0:
             print("** class doesn't exist **")
+            return
         elif len(args) == 1:
             print("** instance id missing **")
+            return
         else:
             key = "{}.{}".format(args[0], args[1])
             new_dict = models.storage.all()
@@ -68,6 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 print(new_dict[key])
             else:
                 print("** no instance found **")
+                return
 
     def do_destroy(self, args):
         """Deletes an instance based on the class name and id"""
@@ -79,8 +85,10 @@ class HBNBCommand(cmd.Cmd):
                 check = 1
         if check == 0:
             print("** class doesn't exist **")
+            return
         elif len(args) == 1:
             print("** instance id missing **")
+            return
         else:
             key = "{}.{}".format(args[0], args[1])
             new_dict = models.storage.all()
@@ -88,6 +96,7 @@ class HBNBCommand(cmd.Cmd):
                 del new_dict[key]
             else:
                 print("** no instance found **")
+                return
 
     def do_update(self, args):
         """
@@ -100,30 +109,38 @@ class HBNBCommand(cmd.Cmd):
             storage.all()[class_id].save()
         elif len(args) == 0:
             print("** class name missing **")
+            return
         elif args[0] not in HBNBCommand.class_names:
             print("** class doesn't exist **")
+            return
         elif len(args) == 1:
             print("** instance id missing **")
+            return
         elif ("{}.{}".format(args[0], args[1])) not in storage.all().keys():
             print("** no instance found **")
+            return
         elif len(args) == 2:
             print("** attribute name missing **")
+            return
         else:
             print("** value missing **")
+            return
 
     def do_all(self, args):
         """
         do_all - prints string representation of all instances
         """
         temp_dict = models.storage.all()
+        if args is "":
+            print([obj for obj in temp_dict.values()])
+            return
         if args not in HBNBCommand.class_names:
             print("** class doesn't exist **")
-        if args == "all":
-            for obj in temp_dict:
-                print([temp_dict[obj]])
+            return
         else:
-            for obj in temp_dict:
-                print([temp_dict[obj]])
+            values = models.storage.all().values()
+            print([obj for obj in values if type(obj).__name__ == args])
+
 
 
 if __name__ == '__main__':
